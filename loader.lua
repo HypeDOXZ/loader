659f23f94238139fa147783c031ca98ba0aeabf3
HypeDOXZ <149960408+HypeDOXZ@users.noreply.github.com>
2026-06-28 12:35:40 -0400

    Update loader to use loadstring for script execution

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-https://raw.githubusercontent.com/HypeDOXZ/denteditz/refs/heads/main/decent.lua?token=GHSAT0AAAAAAEAWFH7J2EN6HDYXM6PJGUTS2SBJUTA
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HypeDOXZ/denteditz/refs/heads/main/decent.lua?token=GHSAT0AAAAAAEAWFH7J2EN6HDYXM6PJGUTS2SBJUTA"))()
