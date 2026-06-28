8c898716254af892ed0d97153516b1f70392d603
HypeDOXZ <149960408+HypeDOXZ@users.noreply.github.com>
2026-06-28 12:43:45 -0400

    Update URL in loader.lua for decent.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/HypeDOXZ/denteditz/refs/heads/main/decent.lua?token=GHSAT0AAAAAAEAWFH7J2EN6HDYXM6PJGUTS2SBJUTA"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HypeDOXZ/denteditz/refs/heads/main/decent.lua?token=GHSAT0AAAAAAEAWFH7IIMYMSET3XKEPK33Y2SBJ5LQ"))()
